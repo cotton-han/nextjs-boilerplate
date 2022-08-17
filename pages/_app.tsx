8936cc4657119b9351fce0ec627ab8e542544878
cotton-han <cotton.dev96@gmail.com>
2022-08-17 17:06:36 +0900

    feat: react-query 설정

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,16 +1,27 @@
 import { ThemeProvider } from '@emotion/react';
-import { AppProps } from 'next/app';
+import { Hydrate, QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { useState } from 'react';
 import { RecoilRoot } from 'recoil';
 
 import { globalStyles } from 'styles/global';
 import theme from 'styles/theme';
 
+import type { AppProps } from 'next/app';
+
 function App({ Component, pageProps }: AppProps) {
+  const [queryClient] = useState(() => new QueryClient());
+
   return (
     <RecoilRoot>
       <ThemeProvider theme={theme}>
         {globalStyles}
-        <Component {...pageProps} />
+        <QueryClientProvider client={queryClient}>
+          <Hydrate state={pageProps.dehydratedState}>
+            <Component {...pageProps} />
+            <ReactQueryDevtools initialIsOpen={false} />
+          </Hydrate>
+        </QueryClientProvider>
       </ThemeProvider>
     </RecoilRoot>
   );
